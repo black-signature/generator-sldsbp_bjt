d6b63e3c1c40cb432b92d73112b03c3c2043acac
Balu John Thomas <balujohnthomas@gmail.com>
2017-06-25 17:12:15 +0530

    Updated LICENSE, bower.json to fetch the latest SLDS, modified gulpfile.js to minify the custom JS

--- generators/app/templates/gulpfile.js
+++ generators/app/templates/gulpfile.js
@@ -2,12 +2,11 @@
  * Gulp file to make the page distributable under dist folder
  * 
  * Author   : Balu John Thomas
- * Version  : 1.0
- * Support  : balu.j.thomas@pwc.com
+ * Version  : 1.4
+ * Support  : balujohnthomas@gmail.com
  */ 
 
 var gulp = require("gulp");
-var concat = require("gulp-concat");
 var uglify = require("gulp-uglify");
 var replace = require('gulp-replace');
 
@@ -37,11 +36,12 @@ gulp.task('copy:other_components', function() {
     .pipe(gulp.dest('dist/plugins'));
 });
 
-/*** Copy Custom Scripts */
+/*** Copy and Minify Custom Scripts */
 gulp.task("copy:scripts", function(){
 	return gulp.src([
 		'js/**'
     ])
+	.pipe(uglify())
     .pipe(gulp.dest('dist/custom/js'));
 });
 
@@ -69,8 +69,8 @@ gulp.task("copy:static_htmls", function(){
     .pipe(gulp.dest('dist'));
 });
 
-/*** Replace SLDS and jQuery paths in all the pages */
-gulp.task("replace:paths_slds_jquery", function(){
+/*** Replace all references within dist */
+gulp.task("replace:path_references", function(){
   gulp.src(['dist/*.html'])
     .pipe(replace('bower_components/salesforce-lightning-design-system/assets/', 'SLDS/'))
     .pipe(replace('bower_components/jquery/dist/', 'lib/'))
@@ -81,6 +81,7 @@ gulp.task("replace:paths_slds_jquery", function(){
     .pipe(gulp.dest('dist/'));
 });
 
+/*** Run tasks sequentially -- alternative till Gulp 4 */
 function runSequential( tasks ) {
     if( !tasks || tasks.length <= 0 ) return;
 
@@ -92,7 +93,5 @@ function runSequential( tasks ) {
 }
 
 gulp.task("default", function(){
-    runSequential(["copy:bower_lightning", "copy:jquery", "copy:scripts", "copy:css", "copy:images", "copy:other_components", "copy:static_htmls", "replace:paths_slds_jquery"]);
+    runSequential(["copy:bower_lightning", "copy:jquery", "copy:scripts", "copy:css", "copy:images", "copy:other_components", "copy:static_htmls", "replace:path_references"]);
 });
-
-//gulp.task("default", ["copy:bower_lightning", "copy:scripts", "copy:css", "copy:images", "copy:static_htmls"]);
